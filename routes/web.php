ca97520aa4ee78d31fedcd3214b6d93bfcd91da9
anwarrkt <anwarrkt890@gmail.com>
2023-11-26 02:55:32 +0700

    push kedua menggunakan github

--- routes/web.php
+++ routes/web.php
@@ -8,26 +8,25 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('index');
 });
 
-Route::get('produk', function () {
-    $kodeproduk = ['BRG001', 'BRG002'];
-    $namaproduk = ['Buku', 'Pena'];
-    $jenisproduk = ['Alat Tulis', 'Alat Tulis'];
-    $harga = [20000, 15000];
-    $jumlah = count($kodeproduk);
-    return view('produk', compact('kodeproduk', 'namaproduk', 'jenisproduk', 'harga', 'jumlah'));
+Route::get('about', function () {
+    return view('about');
 });
 
-Route::get('/tambahproduk', function () {
-    $jenisProduk = ['Jenis Produk', 'Alat Tulis', 'Elektronik', 'Sembako'];
-    $jumlahproduk = count($jenisProduk);
-    return view('tambahproduk', compact('jenisProduk', 'jumlahproduk'));
+Route::get('mahasiswa', function () {
+    return view('mahasiswa');
+});
+
+Route::get('profile', function () {
+    $nama = 'Anwar';
+    //  return view('profile', compact('nama'));
+    return view('profile')->with('nama', $nama);
 });
